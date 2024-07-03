9cb5d1c4a9884c31f1203404daa8e7a445e22147
Anthony Vu Le <leducvuvietnam@gmail.com>
2024-07-03 08:12:54 +0000

    updated build context for docker compose and dockerfile

--- deploy.sh
+++ deploy.sh
@@ -1,19 +1,8 @@
 #/bin/bash
-# To run this file, you must use bash deploy.sh, not sh deploy.sh
-
-DOCKERFILE=Dockerfile
-IMAGE_NAME=stable-diffusion-api:latest
-
-if [[ $DOCKERFILE -nt "$(docker inspect -f '{{.Created}}' $IMAGE_NAME)" ]]; then
-    echo "Dockerfile has changed. Rebuilding $IMAGE_NAME..."
-    docker build -t $IMAGE_NAME .
-else
-    echo "Dockerfile has not changed. Skipping rebuild."
-fi
 
 echo "Dowloading models from get_models.sh. If you wanna add more models, just put links in file and then re-run the deploy.sh command or you can just run sh get_models.sh seperately."
 
 sh get_models.sh
 
 echo "running docker compose up for deploy"
-docker compose up
+docker compose up --force-recreate
