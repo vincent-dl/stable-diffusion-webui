a1e4dd2de313a6ac6c2ec9ef5365ba2b15287e1e
Anthony Vu Le <leducvuvietnam@gmail.com>
2024-07-04 08:52:34 +0000

    added sudo apt-get install -y nvidia-container-toolkit

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,8 @@
 #/bin/bash
 
+echo "Installing nvidia-container-toolkit"
+sudo apt-get install -y nvidia-container-toolkit
+
 echo "Dowloading models from get_models.sh. If you wanna add more models, just put links in file and then re-run the deploy.sh command or you can just run sh get_models.sh seperately."
 
 sh get_models.sh
